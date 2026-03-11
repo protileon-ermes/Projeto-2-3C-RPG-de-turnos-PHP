6d74dd4068f8bf8b469960d36f7281b55b43ab3a
Ermindo Treviso Neto <126285198+protileon-ermes@users.noreply.github.com>
2026-03-11 11:20:43 -0300

    Delete Projeto 2 /src directory

--- Projeto 2 /src/excecoes/EnergiaInsuficienteException.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class EnergiaInsuficienteException extends Exception {
-    public function __construct($mensagem = "Energia insuficiente para esta ação!", $code = 0, Throwable $previous = null) {
-        parent::__construct($mensagem, $code, $previous);
-    }
-}
--- Projeto 2 /src/interfaces/Combatente.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-interface Combatente {
-    public function atacar(Personagem $alvo);
-    public function defender();
-    public function habilidadeEspecial1(Personagem $alvo);
-    public function habilidadeEspecial2(Personagem $alvo);
-}
